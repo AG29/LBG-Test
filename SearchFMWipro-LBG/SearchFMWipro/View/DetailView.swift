4fdf590685c4e4a015e7256f16ee23dada901eec
Ashish Gore <ashishgore29@gmail.com>
2023-12-10 18:27:27 +0530

    Final Commit

---
+++ SearchFMWipro-LBG/SearchFMWipro/View/DetailView.swift
@@ -0,0 +1,25 @@
+//
+//  DetailView.swift
+//  SearchFMWipro
+//
+//  Created by AG on 10/12/23.
+//  Copyright © 2023 AG. All rights reserved.
+//
+
+import UIKit
+
+class DetailView: UIView {
+
+    func reset() {
+        
+    }
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
